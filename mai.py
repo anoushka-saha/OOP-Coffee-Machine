767618a31d6b0982b6e195c32b589959f0a64c92
anoushka1111 <saha.anoushka@gmail.com>
2024-05-12 13:18:26 -0400

    setting up files

---
+++ mai.py
@@ -0,0 +1,9 @@
+#Anoushka Saha
+#OOP Coffee Machine
+#May 12th, 2024
+#Day 16 Final Project
+
+#Importing
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
